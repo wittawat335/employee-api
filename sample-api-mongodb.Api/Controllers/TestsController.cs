22acffff55f3fdd9e689d04517f49143cb914333
Wittawat Fuangfu <wittawat335@gmail.com>
2024-05-02 16:33:04 +0700

    #

---
+++ sample-api-mongodb.Api/Controllers/TestsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace sample_api_mongodb.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestsController : ControllerBase
+    {
+        public string GenerateGreetText()
+        {
+            var dateTimeNow= DateTime.Now;
+            return dateTimeNow.Hour switch
+            { 
+                >= 5 and < 12 => "Morning",
+                >= 12 and < 18 => "Afternoon",
+                _ => "Evering"
+            };
+        }
+    }
+}
